2b298a219d2072a04b7f7cb105ab67738aecd479
toxtli <ctoxtli@gmail.com>
2020-12-10 23:38:16 -0500

    player urls updated

--- js/json.js
+++ js/json.js
@@ -21,21 +21,21 @@ fetch(serverUrl)
 function loadThumbs() {
 	console.log(thumbs[1][0][3]);
 	$('.img-1-0').css("background-image", `url(${thumbs[1][0][3]})`);
-	$('.link-block-hero').attr('href', `/player.html#${thumbs[1][0][0]},${thumbs[1][0][2]}`);
+	$('.link-block-hero').attr('href', `player.html#${thumbs[1][0][0]},${thumbs[1][0][2]}`);
 	$('.p-hero-home').html(thumbs[1][0][1]);
 	for (var i=0; i<6; i++) {
 		$(`.img-2-${i}`).css("background-image", `url(${thumbs[2][i][3]})`);
-		$(`.img-2-${i}`).attr('href', `/player.html#${thumbs[2][i][0]},${thumbs[2][i][2]}`)	
+		$(`.img-2-${i}`).attr('href', `player.html#${thumbs[2][i][0]},${thumbs[2][i][2]}`)	
 		$(`.img-2-${i} .p-charts-home`).html(thumbs[2][i][1]);
 	}
 	for (var i=0; i<6; i++) {
 		$(`.img-3-${i}`).css("background-image", `url(${thumbs[3][i][3]})`);	
-		$(`.img-3-${i}`).attr('href', `/player.html#${thumbs[3][i][0]},${thumbs[3][i][2]}`)
+		$(`.img-3-${i}`).attr('href', `player.html#${thumbs[3][i][0]},${thumbs[3][i][2]}`)
 		$(`.img-3-${i} .p-charts-home`).html(thumbs[3][i][1]);
 	}
 	for (var i=0; i<10; i++) {
 		$(`.img-4-${i} .cover-fav-home`).css("background-image", `url(${thumbs[4][i][3]})`);	
-		$(`.img-4-${i}`).attr('href', `/player.html#${thumbs[4][i][0]},${thumbs[4][i][2]}`)
+		$(`.img-4-${i}`).attr('href', `player.html#${thumbs[4][i][0]},${thumbs[4][i][2]}`)
 		$(`.img-4-${i} .h5-fav-home`).html('');
 		$(`.img-4-${i} .p-fav-home`).html(thumbs[4][i][1]);
 		
